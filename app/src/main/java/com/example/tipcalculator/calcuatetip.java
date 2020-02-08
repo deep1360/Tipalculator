514c9cceafb8049b956d2c5c5381674dca08ad4b
harbir Singh <hsdhilllon0351@gmail.com>
2020-02-07 20:29:29 -0500

    tip page

---
+++ app/src/main/java/com/example/tipcalculator/calcuatetip.java
@@ -0,0 +1,21 @@
+package com.example.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class calcuatetip extends AppCompatActivity {
+
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maincal);
+
+        b1=findViewById(R.id.calculate);
+        b2.findViewById(R.id.button2);
+        
+    }
+}
