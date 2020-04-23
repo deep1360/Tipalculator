2076e294f1b387ce89ddfe38480dc16e329e5513
amandeep <adeeps1360@gmail.com>
2020-04-23 13:52:54 -0400

    mysql

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -11,11 +11,22 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +34,11 @@ public class MainActivity extends AppCompatActivity {
     EditText password;
     Button login;
     TextView register;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
         username= findViewById(R.id.username);
         password=findViewById(R.id.password);
 
@@ -41,8 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
  getuser();
-                Intent i = new Intent(MainActivity.this, calcuatetip.class);
-                startActivity(i);
+
             }
         });
 
@@ -59,21 +67,63 @@ public class MainActivity extends AppCompatActivity {
     {
         String userN=username.getText().toString().trim();
         String pass=password.getText().toString().trim();
+        loginMethod(userN, pass) ;
 
-        mAuth.signInWithEmailAndPassword(userN, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                Toast.makeText(MainActivity.this, "Success .",
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
-            }
-        });
 
     }
+
+    public void loginMethod(final String stEmail, final String stPassword) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://10.0.2.2:3012/login",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+
+                           // String status = response;
+                            if (response.contains("Valid user")) {
+                                Intent i = new Intent(MainActivity.this, calcuatetip.class);
+                                startActivity(i);
+                            }
+                            Toast.makeText(MainActivity.this,response,Toast.LENGTH_SHORT).show();
+
+                        } catch (Exception e) {
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        String error_msg = "something went wrong";
+                        Toast.makeText(MainActivity.this,error_msg,Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("email", stEmail);
+                map.put("password", stPassword);
+                return map;
+            }
+
+        };
+        RequestQueue requestQueue1 = Volley.newRequestQueue(this);
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                60000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue1.add(stringRequest);
+    }
+
 }
--- app/src/main/java/com/example/tipcalculator/RegisterActivity.java
+++ app/src/main/java/com/example/tipcalculator/RegisterActivity.java
@@ -11,6 +11,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -29,12 +37,10 @@ public class RegisterActivity extends AppCompatActivity {
     EditText confirmpassword;
     Button signup ;
     TextView login1;
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.registeration);
         username1=findViewById(R.id.username1);
         password1=findViewById(R.id.password1);
@@ -45,8 +51,9 @@ public class RegisterActivity extends AppCompatActivity {
         login1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(RegisterActivity.this,MainActivity.class);
-               startActivity(i);
+                Intent i = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(i);
+
             }
         });
 
@@ -55,8 +62,7 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 createUser();
 
-                Intent i = new Intent(RegisterActivity.this,MainActivity.class);
-                startActivity(i);
+
             }
         });
 
@@ -67,42 +73,62 @@ public class RegisterActivity extends AppCompatActivity {
     {
         String username=username1.getText().toString().trim();
         String password=password1.getText().toString().trim();
+        registerMethod(username,password);
 
-        mAuth.createUserWithEmailAndPassword(username, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+    }
+
+    public void registerMethod(final String stEmail, final String stPassword) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://10.0.2.2:3012/register",
+                new Response.Listener<String>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(RegisterActivity.this, "Success .",
-                                    Toast.LENGTH_SHORT).show();
-                            FirebaseFirestore db = FirebaseFirestore.getInstance();
-                            String user=username1.getText().toString().trim();
-                            String pass= password1.getText().toString().trim();
-                            String confirm = confirmpassword.getText() .toString().trim();
+                    public void onResponse(String response) {
+                        try {
 
-                            Map<String, Object> myuser = new HashMap<>();
-                            myuser.put("username", user);
-                            myuser.put("password", pass);
-                            myuser.put("confirmpassword", confirm);
-                            db.collection("users").add(myuser).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Toast.makeText(getApplicationContext(),"added",Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                            // String status = response;
+
+                            Toast.makeText(RegisterActivity.this,response,Toast.LENGTH_SHORT).show();
+                            Intent i=new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(i);
+
+                        } catch (Exception e) {
 
                         }
-
-                        // ...
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        String error_msg = "something went wrong";
+                        Toast.makeText(RegisterActivity.this,error_msg,Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
             @Override
-            public void onFailure(@NonNull Exception e)
-            {
-               Toast.makeText(RegisterActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-               e.printStackTrace();
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
+                return headers;
             }
-        });
+
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("email", stEmail);
+                map.put("password", stPassword);
+                return map;
+            }
+
+        };
+        RequestQueue requestQueue1 = Volley.newRequestQueue(this);
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                60000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue1.add(stringRequest);
     }
+
+
 }
